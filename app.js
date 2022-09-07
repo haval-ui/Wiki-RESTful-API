73d21cae1f8c9e8c39557c97856e71799382f8c6
haval <havalkoet00908@gmail.com>
2022-09-07 01:18:58 -0700

    fix bugs

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 // require the wanted files 
 const express = require('express');
 const mongoose=require('mongoose');
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const ejs =require('ejs');
 //set up the server
+
 const app = express() ;
 const port = 3000 ;
 
 mongoose.connect('mongodb://localhost:27017/wikiDB');
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //create the mongoose schema and model
 
-const articlesSchema= new mongoose.Schema({
+const articleSchema= new mongoose.Schema({
     title:String,
     connect:String,
 });
 
-const Article=mongoose.model("Article",articlesSchema);
+const Article=mongoose.model("Article",articleSchema);
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/articles', (req, res) =>{ 
+    
+    Article.find({},(err,foundArticles)=>{
+        if(!err){
+            res.send(foundArticles)
+        }else{
+            console.log(err)
+        }
+        
+    })
+
+});
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
